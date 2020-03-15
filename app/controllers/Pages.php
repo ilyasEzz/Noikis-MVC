4b00ca778c15ba68c5c6ddbb078e0abd9c7e2326
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-03-15 13:17:48 +0700

    class:: Route

---
+++ app/controllers/Pages.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Pages {
+
+	public function index() {
+	}
+
+  public function edit($id) {
+		echo $id;
+  }
+}
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+// Load Libraries
+require_once '../app/libraries/Route.php';
+require_once '../app/libraries/Controller.php';
+require_once '../app/libraries/Database.php';
+
+// Inititialisation
+$init = new Route;
